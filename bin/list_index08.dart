e10765d42b5306b4cbf99bb8ec88e8771f252e1e
Mirjalol <jalolubaydullayev575@gmail.com>
2023-05-15 14:00:14 +0500

    08

--- bin/list_index08.dart
+++ bin/list_index08.dart
@@ -6,4 +6,20 @@
     Returns:
         list: return answer
 */
-void main() {}
+List func(List list1) {
+  int x = 0;
+  while (x < list1.length) {
+    if (list1[x] > 0) {
+      list1[x] = true;
+      x += 1;
+    } else {
+      list1[x] = false;
+      x += 1;
+    }
+  }
+  return list1;
+}
+
+void main() {
+  print(func([1,0,1,0,1]));
+}
